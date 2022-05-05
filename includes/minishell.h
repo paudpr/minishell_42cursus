d01c9f35d63483734a22563eb7961ea55b9a34bb
Paula <paula.p.ramirez@gmail.com>
2022-05-05 19:16:04 +0200

    .

---
+++ includes/minishell.h
@@ -0,0 +1,15 @@
+#ifndef MINISHELL_H
+# define MMINISHELL_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <readline/readline.h>
+#include <readline/history.h>
+
+
+
+
+
+#endif
--- sources/readline.c
+++ sources/readline.c
@@ -4,20 +4,23 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+
+
 int main(void)
 {
 	char *line;
 	char *eof;
 
-	eof = "eof";
+	eof = "exit";
 
-	line = readline("test > ");
-	while(line != eof)
+	line = readline("minishell > ");
+	while(strncmp(line, eof, strlen(eof)) != 0)
 	{
-		line = readline("loquesea -> ");
+		line = readline("minishell > ");
 		if(line != NULL)
 			add_history(line);
 		free(line);
 	}
 	return(0);
 }
+
